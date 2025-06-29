76a57f0a0fabaf7dd7288c2dbd6c6b17c01345d1
Akinyele Ibrahim <65444509+Akinyeleib@users.noreply.github.com>
2025-06-29 08:19:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,12 @@ def index():
         })
     
     # Render the template for GET request
-    return render_template('index.html')
+    # return render_template('index.html')
+
+    return jsonify({
+        'success': True,
+        'message': 'Server working!'
+    })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0')
